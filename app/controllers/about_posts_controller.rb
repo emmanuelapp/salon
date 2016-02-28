3af0fa07a416b8cc1154c1c5517cfe8e3c57bdcb
Georgi Kostov <internal.sun@yahoo.bg>
2016-02-28 13:27:59 +0200

    add new action and a view

--- app/controllers/about_posts_controller.rb
+++ app/controllers/about_posts_controller.rb
@@ -2,4 +2,12 @@ class AboutPostsController < ApplicationController
   def index
     @about_posts = AboutPost.all
   end
+
+  def new
+    @about_post = AboutPost.new
+  end
+
+  def create
+    render text: "TODO"
+  end
 end
