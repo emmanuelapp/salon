e729b13e23a36d0b94e002555eb5692eef68b6e0
Georgi Kostov <internal.sun@yahoo.bg>
2016-02-28 13:51:58 +0200

    add/refactor views

--- app/controllers/about_posts_controller.rb
+++ app/controllers/about_posts_controller.rb
@@ -1,4 +1,6 @@
 class AboutPostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update]
+
   def index
     @about_posts = AboutPost.all
   end
@@ -8,6 +10,36 @@ class AboutPostsController < ApplicationController
   end
 
   def create
-    render text: "TODO"
+    @about_post = AboutPost.new(about_post_params)
+
+    if @about_post.save
+      flash[:notice] = "Публикацията е успешно запаметена!"
+    else
+      flash[:notice] = "Възникна грешка!!"
+    end
+
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+  	if @about_post.update(about_post_params)
+  	  flash[:notice] = "Успешна редакция на публикацията"
+  	else
+  	  flash[:notice] = "Възникна грешка!!"
+  	end
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+  	@about_post = AboutPost.find(params[:id])
+  end
+
+  def about_post_params
+  	params.require(:about_post).permit(:title, :body)
   end
 end
