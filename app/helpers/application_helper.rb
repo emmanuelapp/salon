438ddd0ec7538dff380a2ae0555009ff884a21ae
Georgi N. Kostov <internal.sun@yahoo.bg>
2016-09-03 22:30:58 +0300

    extract logic to helper modules

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # :nodoc:
 module ApplicationHelper
+  def list_items(collection)
+    if collection.empty?
+      '<h5>This page is empty</h5>'.html_safe
+    else
+      render collection
+    end
+  end
 end
