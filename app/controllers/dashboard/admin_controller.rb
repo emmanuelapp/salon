34f72a9b3219e1c470065e6f7461cd3ae58d2d58
Georgi N. Kostov <internal.sun@yahoo.bg>
2016-10-23 12:45:15 +0300

    delete redundant comments

--- app/controllers/dashboard/admin_controller.rb
+++ app/controllers/dashboard/admin_controller.rb
@@ -1,5 +1,4 @@
 module Dashboard
-  # This class contains functionality that is common for all admin controllers.
   class AdminController < ApplicationController
     layout 'dashboard'
     before_action :authenticate_user!
