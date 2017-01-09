cfbcc2fecb40a8a10caf0c1249f8dc7cd156ecbe
Georgi N. Kostov <gnkostov@yahoo.com>
2017-01-09 21:04:51 +0200

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20161023132917) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "abouts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "bookings", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
