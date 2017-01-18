0d9a2df149e93d04c4c1a7ceeff7c894c61690f5
Georgi N. Kostov <gnkostov@yahoo.com>
2017-01-19 01:34:27 +0200

    Edit views
    Improve styles, remove redundant logic from helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 # :nodoc:
 module ApplicationHelper
-  def list_items(collection)
-    if collection.empty?
-      '<h5>This page is empty</h5>'.html_safe
-    else
-      render collection
-    end
-  end
 end
