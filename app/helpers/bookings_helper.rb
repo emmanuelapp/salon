3669b1f9a781039eeb426406a62f72d2687980af
Georgi N. Kostov <gnkostov@yahoo.com>
2017-02-12 13:43:54 +0200

    Extract logic from bookings/_form.html.erb

---
+++ app/helpers/bookings_helper.rb
@@ -0,0 +1,36 @@
+module BookingsHelper
+  def reserved_at_dropdown(form_builder)
+    form_builder.datetime_select :reserved_at,
+                                 reserved_at_options,
+                                 reserved_at_html_options
+  end
+
+  def reserved_at_options
+    {
+      order: [:day, :month, :year],
+      start_year: Time.now.year,
+      end_year: Time.now.year + 1,
+      prompt: reserved_at_prompt,
+      datetime_separator: '',
+      time_separator: '',
+      minute_step: 15
+    }
+  end
+
+  def reserved_at_prompt
+    {
+      day: t(:select_day),
+      month: t(:select_month),
+      year: t(:select_year),
+      hour: t(:select_hour),
+      minute: t(:select_minute)
+    }
+  end
+
+  def reserved_at_html_options
+    {
+      class: 'form-control',
+      required: true
+    }
+  end
+end
