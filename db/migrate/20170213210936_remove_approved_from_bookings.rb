4f6a4ba7f8a0e033a0b00fccc1b38b2d58dc807f
Georgi N. Kostov <gnkostov@yahoo.com>
2017-02-13 23:11:23 +0200

    Recreate Bookings#approved flag
    Set default value for Bookings#approved to false.

---
+++ db/migrate/20170213210936_remove_approved_from_bookings.rb
@@ -0,0 +1,5 @@
+class RemoveApprovedFromBookings < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :bookings, :approved
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212111119) do
+ActiveRecord::Schema.define(version: 20170213211027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 20170212111119) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
-    t.boolean  "approved"
     t.text     "additional_info"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.datetime "reservation"
     t.datetime "reserved_at"
+    t.boolean  "approved",        default: false
   end
 
   create_table "members", force: :cascade do |t|
