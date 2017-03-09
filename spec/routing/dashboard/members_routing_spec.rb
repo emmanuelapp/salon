0154479465f4c7346d9e98f623b25a86308d791d
Georgi N. Kostov <gnkostov@yahoo.com>
2017-03-10 01:27:59 +0200

    Uncomment spec code which was temporarily skipped

--- spec/routing/dashboard/members_routing_spec.rb
+++ spec/routing/dashboard/members_routing_spec.rb
@@ -2,37 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Dashboard::MembersController, type: :routing do
   describe 'routing' do
-    # TODO
-    # it "routes to #index" do
-    #   expect(:get => "/dashboard/members").to route_to("dashboard/members#index")
-    # end
+    it 'routes to #index' do
+      path = { get: '/dashboard/members' }
+      expect(path).to route_to('dashboard/members#index')
+    end
 
-    # it "routes to #new" do
-    #   expect(:get => "/dashboard/members/new").to route_to("dashboard/members#new")
-    # end
+    it 'routes to #new' do
+      path = { get: '/dashboard/members/new' }
+      expect(path).to route_to('dashboard/members#new')
+    end
 
-    # it "routes to #show" do
-    #   expect(:get => "/dashboard/members/1").to route_to("dashboard/members#show", :id => "1")
-    # end
+    it 'routes to #edit' do
+      path = { get: '/dashboard/members/1/edit' }
+      expect(path).to route_to('dashboard/members#edit', id: '1')
+    end
 
-    # it "routes to #edit" do
-    #   expect(:get => "/dashboard/members/1/edit").to route_to("dashboard/members#edit", :id => "1")
-    # end
+    it 'routes to #create' do
+      path = { post: '/dashboard/members' }
+      expect(path).to route_to('dashboard/members#create')
+    end
 
-    # it "routes to #create" do
-    #   expect(:post => "/dashboard/members").to route_to("dashboard/members#create")
-    # end
+    it 'routes to #update via PUT' do
+      path = { put: '/dashboard/members/1' }
+      expect(path).to route_to('dashboard/members#update', id: '1')
+    end
 
-    # it "routes to #update via PUT" do
-    #   expect(:put => "/dashboard/members/1").to route_to("dashboard/members#update", :id => "1")
-    # end
+    it 'routes to #update via PATCH' do
+      path = { patch: '/dashboard/members/1' }
+      expect(path).to route_to('dashboard/members#update', id: '1')
+    end
 
-    # it "routes to #update via PATCH" do
-    #   expect(:patch => "/dashboard/members/1").to route_to("dashboard/members#update", :id => "1")
-    # end
-
-    # it "routes to #destroy" do
-    #   expect(:delete => "/dashboard/members/1").to route_to("dashboard/members#destroy", :id => "1")
-    # end
+    it 'routes to #destroy' do
+      path = { delete: '/dashboard/members/1' }
+      expect(path).to route_to('dashboard/members#destroy', id: '1')
+    end
   end
 end
