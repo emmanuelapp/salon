8c1b745f62e932fd717334ca8181bf311c44ce67
Georgi N. Kostov <gnkostov@yahoo.com>
2017-03-10 23:12:04 +0200

    Add specs for OffersHelper

---
+++ spec/helpers/offers_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe OffersHelper, type: :helper do
+  describe '#format_price' do
+    context 'when offer price is 123.999999' do
+      let(:offer) { create(:offer, price: 123.999999) }
+
+      it 'returns 124.00 BGN' do
+        expect(helper.format_price(offer)).to eq '124.00 BGN'
+      end
+    end
+
+    context 'when offer price is 123.9' do
+      let(:offer) { create(:offer, price: 123.9) }
+
+      it 'returns 124.00 BGN' do
+        expect(helper.format_price(offer)).to eq '123.90 BGN'
+      end
+    end
+  end
+end
