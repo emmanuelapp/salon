08fbad68da7b3a4288cc1c158233d33e1ce8a4cb
Georgi Kostov <gnkostov@yahoo.com>
2017-04-02 15:28:08 +0300

    Add missing controller specs

---
+++ spec/controllers/paragraphs_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ParagraphsController, type: :controller do
+  describe 'GET #index' do
+    subject { get :index }
+
+    it 'responds with HTTP status 200' do
+      expect(subject).to have_http_status(200)
+    end
+
+    it 'renders template index' do
+      expect(subject).to render_template(:index)
+    end
+  end
+end
