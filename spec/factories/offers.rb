d6062b3497f20441bfccea6db92511533af65d19
Georgi Kostov <gnkostov@yahoo.com>
2017-04-11 01:21:14 +0300

    Add missing model specs

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -1,4 +1,13 @@
 FactoryGirl.define do
   factory :offer do
+    trait :with_price do
+      price 12.22
+    end
+
+    trait :with_name do
+      name 'Relaxing massage'
+    end
+
+    factory :offer_with_name_and_price, traits: [:with_price, :with_name]
   end
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Offer, type: :model do
+  describe '#to_s' do
+    subject { create(:offer_with_name_and_price) }
+
+    it 'returns the expected string' do
+      expect(subject.to_s).to eq '12.22 - Relaxing massage'
+    end
+
+    it 'is an instance of String' do
+      expect(subject.to_s).to be_an_instance_of(String)
+    end
+  end
 end
