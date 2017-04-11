29002fd17a680591959d2dcee9394df341479645
Georgi Kostov <gnkostov@yahoo.com>
2017-04-11 23:43:41 +0300

    Delete deprecated code

--- app/controllers/dashboard/booking_weeks_controller.rb
+++
@@ -1,8 +0,0 @@
-module Dashboard
-  # :nodoc:
-  class BookingWeeksController < AdminController
-    def index
-      render component: 'BookingWeek', props: { this_week: Booking.this_week }
-    end
-  end
-end
