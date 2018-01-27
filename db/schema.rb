4d9755d0580bb4483a705e3ba504e65afaea1529
Georgi N. Kostov <joromir@protonmail.com>
2018-01-27 10:18:02 +0200

    transition to the default PG config for database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -15,72 +15,72 @@ ActiveRecord::Schema.define(version: 20170415101844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.string   "first_name",      default: "",    null: false
-    t.string   "last_name",       default: "",    null: false
-    t.string   "phone",           default: "",    null: false
-    t.text     "additional_info", default: ""
-    t.boolean  "confirmed",       default: false
-    t.datetime "reserved_at",                     null: false
-    t.integer  "week_number"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.integer  "offer_id"
+  create_table "bookings", id: :serial, force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "phone", default: "", null: false
+    t.text "additional_info", default: ""
+    t.boolean "confirmed", default: false
+    t.datetime "reserved_at", null: false
+    t.integer "week_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "offer_id"
   end
 
-  create_table "members", force: :cascade do |t|
-    t.string   "first_name",          default: "", null: false
-    t.string   "last_name",           default: "", null: false
-    t.string   "profession",          default: "", null: false
-    t.string   "phone_number",        default: "", null: false
-    t.text     "description",         default: "", null: false
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+  create_table "members", id: :serial, force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "profession", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.text "description", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "slug"
+    t.string "slug"
   end
 
-  create_table "offers", force: :cascade do |t|
-    t.text     "description", default: "",    null: false
-    t.string   "name",        default: "",    null: false
-    t.decimal  "price",       default: "0.0", null: false
-    t.integer  "member_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
-
-  create_table "paragraphs", force: :cascade do |t|
-    t.string   "title",      default: "", null: false
-    t.text     "body",       default: "", null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.integer  "booking_id"
-    t.integer  "offer_id"
+  create_table "offers", id: :serial, force: :cascade do |t|
+    t.text "description", default: "", null: false
+    t.string "name", default: "", null: false
+    t.decimal "price", default: "0.0", null: false
+    t.integer "member_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "paragraphs", id: :serial, force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.text "body", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", id: :serial, force: :cascade do |t|
+    t.integer "booking_id"
+    t.integer "offer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
